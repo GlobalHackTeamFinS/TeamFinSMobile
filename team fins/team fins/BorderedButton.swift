d997a435332d3b4ae573db854d1fdb7a78656dbd
Trey Baugher <trey.baugher@gmail.com>
2016-10-22 13:31:40 -0500

    Bordered button class and required framework adds

---
+++ team fins/team fins/BorderedButton.swift
@@ -0,0 +1,28 @@
+//
+//  BorderedButton.swift
+//  team fins
+//
+//  Created by Trey Baugher on 10/22/16.
+//  Copyright © 2016 Team Fins. All rights reserved.
+//
+
+import UIKit
+
+class BorderedButton: UIButton {
+    public var roundRectCornerRadius: CGFloat = 4.0 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        layoutRoundRectLayer()
+    }
+    
+    private func layoutRoundRectLayer() {
+        layer.cornerRadius = roundRectCornerRadius
+        layer.borderWidth = 2.0
+        layer.borderColor = InterfaceTheme.keyColor()
+    }
+}
